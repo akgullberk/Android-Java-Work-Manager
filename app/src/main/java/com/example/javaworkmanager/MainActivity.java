8dfe51af474abd76f7dd7d60a46505787b68bb3c
Berk Akgül <berkak_gul51@hotmail.com>
2024-08-15 17:13:57 +0300

    Chaining

--- app/src/main/java/com/example/javaworkmanager/MainActivity.java
+++ app/src/main/java/com/example/javaworkmanager/MainActivity.java
@@ -7,11 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.Observer;
 import androidx.work.Constraints;
 import androidx.work.Data;
 import androidx.work.NetworkType;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 import androidx.work.WorkRequest;
 
@@ -54,6 +56,35 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         WorkManager.getInstance(this).enqueue(workRequest);
+
+        WorkManager.getInstance(this).getWorkInfoByIdLiveData(workRequest.getId()).observe(this, new Observer<WorkInfo>() {
+            @Override
+            public void onChanged(WorkInfo workInfo) {
+                if(workInfo.getState() == WorkInfo.State.RUNNING){
+                    System.out.println("running");
+                }else if(workInfo.getState() == WorkInfo.State.SUCCEEDED){
+                    System.out.println("succeeded");
+                }else if (workInfo.getState() == WorkInfo.State.FAILED){
+                    System.out.println("Failed");
+                }
+            }
+        });
+
+        //WorkManager.getInstance(this).cancelAllWork();
+
+        /*
+        OneTimeWorkRequest oneTimeWorkRequest = new OneTimeWorkRequest.Builder(RefreshDatabase.class)
+                .setInputData(data)
+                .setConstraints(constraints)
+                .build();
+
+        WorkManager.getInstance(this).beginWith(oneTimeWorkRequest)
+                .then(oneTimeWorkRequest)
+                .then(oneTimeWorkRequest)
+                .enqueue();
+
+
+         */
 
     }
 }
